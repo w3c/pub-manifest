54eecfa5988502ae0ba89f0d498eb70b88721e32
Matt Garrish <matt.garrish@gmail.com>
2019-11-01 21:27:47 -0300

    add pub-manifest uri for testing

--- experiments/manifest-to-internal-processor/manifestProcessor.js
+++ experiments/manifest-to-internal-processor/manifestProcessor.js
@@ -9,7 +9,8 @@ var manifestProcessor = (function() {
 	var dateTime = new RegExp('^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])(T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]{3})?(Z)?)?$');
 	
 	var knownProfiles = {
-		'https://www.w3.org/TR/audiobooks': 'audiobooks'
+		'https://www.w3.org/TR/audiobooks': 'audiobooks',
+		'https://www.w3.org/TR/pub-manifest/': 'generic'
 	};
 	
 	var bodyRelValues = new Set('contents', 'cover', 'pagelist');
