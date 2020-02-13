d9426da49848fc08186f9ee54c4f21f58d6708ac
Matt Garrish <matt.garrish@gmail.com>
2020-02-13 09:08:33 -0400

    intergrate toc processing code and simplify event sequencing by writing out all the promises

--- experiments/manifest-to-internal-processor/README.md
+++ experiments/manifest-to-internal-processor/README.md
@@ -1,20 +1,35 @@
 # Publication Manifest Processor
 
-The publication manifest processor is a JavaScript implementation of the processing algorithm defined in Publication Manifest specification. It also incorporates the extension rules for processing Audiobooks.
+The publication manifest processor is a JavaScript implementation of the processing algorithm defined
+in Publication Manifest specification. It also incorporates the extension rules for processing
+Audiobooks.
 
-The processor returns both the internal representation of the manifest data and any warning and errors that were generated during processing.
+The processor returns both the internal representation of the manifest data and any warning and
+errors that were generated during processing.
 
 ## Setup
 
-The manifest processor code is contained in the [manifestProcessor.js](manifestProcessor.js) file. This file also requires the jQuery library (known to run with 2.2.4) for making ajax calls to obtain an external manifest as well as the [duration.js](https://github.com/evanisnor/durationjs) library for parsing duration values.
+The manifest processor code is contained in the [manifestProcessor.js](manifestProcessor.js) file.
+This file also requires the jQuery library (known to run with 2.2.4) for making ajax calls to obtain
+an external manifest as well as the [duration.js](https://github.com/evanisnor/durationjs) library
+for parsing duration values.
+
+To run the code, call `manifestProcessor.processManifest()`. Create a function called `initPublication`
+to run your initialization code; this function is called after the manifest has been processed.
+
+Example initialization
+```
+manifestProcessor.processManifest();
+
+function initPublication() {
+   var internalRep = manifestProcessor.getInternalRep(); // get the processed manifest
+   var toc = manifestProcessor.getTOC(); // get the processed table of contents
+
+   // initialize publication
+}
+```
 
 ## Implementations
 
 - [JSON testing tool](https://w3c.github.com/pub-manifest/experiments/manifest-to-internal-processor/index.html) &#8212; Interactive tool that returns the result of processing a JSON manifest.
 - [Publication testing tool](https://w3c.github.com/pub-manifest/experiments/manifest-to-internal-processor/linked.html) &#8212; Basic setup for testing an HTML page with a linked or embedded manifest and with support for toc processing.
-
-## Test Suite Results
-
-The [automated results of running the Publication Manifest and Audiobooks test suites](https://w3c.github.com/pub-manifest/experiments/manifest-to-internal-processor/testsuite_results.html) against the processor are available for review. Note that the tests are not ordered within the document due the nature of the AJAX calls involved.
-
-Please **do not open** the testsuite_run.html file. This page dynamically re-runs the test suite tests each time it is accessed.
--- experiments/manifest-to-internal-processor/manifestProcessor.js
+++ experiments/manifest-to-internal-processor/manifestProcessor.js
@@ -3,6 +3,15 @@
 
 var manifestProcessor = (function() {
 
+	var _internal_rep = null;
+	var _manifest_link = '';
+	var _toc = null;
+	
+	var _issues = {
+		'warnings' : [],
+		'errors' : []
+	};
+	
 	var duration = new RegExp('^P([0-9]+(?:[,\.][0-9]+)?Y)?([0-9]+(?:[,\.][0-9]+)?M)?([0-9]+(?:[,\.][0-9]+)?D)?(?:T([0-9]+(?:[,\.][0-9]+)?H)?([0-9]+(?:[,\.][0-9]+)?M)?([0-9]+(?:[,\.][0-9]+)?S)?)?$');
 	var bcp47 = new RegExp('^((?:(en-GB-oed|i-ami|i-bnn|i-default|i-enochian|i-hak|i-klingon|i-lux|i-mingo|i-navajo|i-pwn|i-tao|i-tay|i-tsu|sgn-BE-FR|sgn-BE-NL|sgn-CH-DE)|(art-lojban|cel-gaulish|no-bok|no-nyn|zh-guoyu|zh-hakka|zh-min|zh-min-nan|zh-xiang))|((?:([A-Za-z]{2,3}(-(?:[A-Za-z]{3}(-[A-Za-z]{3}){0,2}))?)|[A-Za-z]{4}|[A-Za-z]{5,8})(-(?:[A-Za-z]{4}))?(-(?:[A-Za-z]{2}|[0-9]{3}))?(-(?:[A-Za-z0-9]{5,8}|[0-9][A-Za-z0-9]{3}))*(-(?:[0-9A-WY-Za-wy-z](-[A-Za-z0-9]{2,8})+))*(-(?:x(-[A-Za-z0-9]{1,8})+))?)|(?:x(-[A-Za-z0-9]{1,8})+))$');
 	// regex works for general cases, but haven't tested in depth 
@@ -132,109 +141,50 @@ var manifestProcessor = (function() {
 			test = init.test;
 		}
 		
-		var manifest_link = '';
-		
 		if (init.hasOwnProperty('manifest_link')) {
-			manifest_link = init.manifest_link;
+			_manifest_link = init.manifest_link;
 		}
 		
 		else {
 			var mlink = document.querySelector('link[rel="publication"]');
 			if (mlink) {
-				manifest_link = mlink.href;
+				_manifest_link = mlink.href;
 			}
 		}
 		
-		if (!manifest_link) {
+		if (!_manifest_link) {
 			throw new Error('Manifest link could not be found.');
 		}
 		
-		return new Promise(function(resolve, reject) {
-			
-			getManifest(manifest_link)
-				.then(function(data) {
-					
-					var _issues = {
-						'warnings' : [],
-						'errors' : []
-					};
-					
-					(function(){
-						var warn = console.warn;
-						console.warn = function (msg) {
-							_issues.warnings.push(msg);
-							warn.apply(console, arguments);
-						};
-						var error = console.error;
-						console.error = function (msg) {
-							_issues.errors.push(msg);
-							error.apply(console, arguments);
-						};
-					})();
-					
-					if (!data) {
-						console.error('Manifest text not found.');
-						throw new Error()
-					}
-					
-					var base = '';
-					var doc_path = [location.protocol, '//', location.host, location.pathname].join('');;
-					
-					if (manifest_link.indexOf(doc_path) == 0 || manifest_link.indexOf('#') == 0) {
-						// manifest is embedded
-						base = location.href.substring(0, location.href.lastIndexOf("/")+1);
-						
-						var baseElem = document.getElementsByTagName("base");
-						
-						if (baseElem.length) {
-							base = baseElem[0].href;
-						}
-					}
-					
-					else  {
-						// use the manifest path
-						base = manifest_link.substring(0, location.href.lastIndexOf("/")+1);
-					}
-					
-					
-					var doc = document;
-					
-					var internal_rep = processManifestData(data, base, doc);
-					resolve({'internal_rep': internal_rep, 'issues': _issues, 'manifest_link': manifest_link, 'test': test});
-				})
-				.catch (function(err) {
-					reject({'error': err, 'manifest_link': manifest_link, 'test': test});
-				})
-		});
+		getManifest();
+		
 	}
 	
 	
-	function getManifest(manifest_link) {
+	function getManifest() {
 		
 		return new Promise(function(resolve, reject) {
 		
-			if (manifest_link) {
-				if (manifest_link.indexOf('#') != -1) {
-					var script = document.getElementById(manifest_link.substring(manifest_link.indexOf('#')+1));
+			if (_manifest_link) {
+				if (_manifest_link.indexOf('#') != -1) {
+					var script = document.getElementById(_manifest_link.substring(_manifest_link.indexOf('#')+1));
 					if (script) {
 						var data = script.innerHTML.trim();
-						resolve(data);
+						generateInternalRep(data);
 					}
 					else {
-						console.error('Manifest ' + manifest_link + ' could not be located');
-						reject('Manifest could not be located');
+						console.error('Manifest ' + _manifest_link + ' could not be located');
 					}
 				}
 				else {
 					$.ajax({
-						url:       manifest_link,
+						url:       _manifest_link,
 						cache:     false,
 						success: function(data) {
-							resolve(JSON.stringify(data));
+							generateInternalRep(JSON.stringify(data));
 						},
 						error: function(xhr, status, error) {
-							console.error('Manifest ' + manifest_link + ' not found or an error occurred retrieving.');
-							reject('Manifest not found or an error occurred retrieving.');
+							console.error('Manifest ' + _manifest_link + ' not found or an error occurred retrieving.');
 						}
 					});
 				}
@@ -242,10 +192,58 @@ var manifestProcessor = (function() {
 			
 			else {
 				console.error('Manifest link could not be located');
-				reject('Manifest link could not be located');
 			}
 		});
 	}
+	
+	
+	
+	function generateInternalRep(data) {
+		
+		(function(){
+			var warn = console.warn;
+			console.warn = function (msg) {
+				_issues.warnings.push(msg);
+				warn.apply(console, arguments);
+			};
+			var error = console.error;
+			console.error = function (msg) {
+				_issues.errors.push(msg);
+				error.apply(console, arguments);
+			};
+		})();
+		
+		if (!data) {
+			console.error('Manifest text not found.');
+			throw new Error();
+		}
+		
+		var base = '';
+		var doc_path = [location.protocol, '//', location.host, location.pathname].join('');;
+		
+		if (_manifest_link.indexOf(doc_path) == 0 || _manifest_link.indexOf('#') == 0) {
+			// manifest is embedded
+			base = location.href.substring(0, location.href.lastIndexOf("/")+1);
+			
+			var baseElem = document.getElementsByTagName("base");
+			
+			if (baseElem.length) {
+				base = baseElem[0].href;
+			}
+		}
+		
+		else  {
+			// use the manifest path
+			base = _manifest_link.substring(0, location.href.lastIndexOf("/")+1);
+		}
+		
+		var doc = document;
+		
+		processManifestData(data, base, doc);
+		
+		processTOC();
+	}
+	
 	
 	
 	function processManifestData(text, base, doc) {
@@ -454,7 +452,7 @@ var manifestProcessor = (function() {
 		
 		// step 10 - return finished data 
 		
-		return processed;
+		_internal_rep = processed;
 	
 	}
 	
@@ -1497,9 +1495,281 @@ var manifestProcessor = (function() {
 	}
 
 
+    function processTOC() {
+    
+    	var toc_markup;
+    	
+    	// get the toc resource
+    	
+    	var resources = _internal_rep.hasOwnProperty('readingOrder') ? _internal_rep.readingOrder : [];
+    		Array.prototype.push.apply(resources, _internal_rep.hasOwnProperty('resources') ? _internal_rep.resources : []);
+    	
+    	var toc_str = '';
+    	var toc_resource = '';
+    	
+    	for (var i = 0; i < resources.length; i++) {
+    		if (resources[i].hasOwnProperty('rel')) {
+    			for (var j = 0; j < resources[i].rel.length; j++) {
+    				if (resources[i].rel[j].toLowerCase() == 'contents') {
+    					toc_resource = resources[i].url;
+    				}
+    			}
+    		}
+    	}
+    	
+    	if (toc_resource) {
+			getTOC(toc_resource);
+    	}
+    	
+    	else {
+    		var toc_element = document.querySelector("*[role~='doc-toc']");
+			generateTOC(toc_element);
+    	}
+    }
+	
+	
+	function generateTOC(toc) {
+		if (toc) {
+			parseTOC(toc,null,null);
+		}
+		else {
+			console.error('No table of contents found.');
+		}
+		initPublication();
+	}
+	
+	
+	function parseTOC(root, enter, exit) {
+		
+		var node = root;
+		
+		var toc = new Object();
+		var stack = new Array();
+		var current_toc_branch = null;
+		
+		start: while (node) {
+			
+			if (node == root) {
+				toc.name = '';
+				toc.entries = new Array();
+			}
+			else {
+				// only interested in processing start element nodes
+				if (node.nodeType == 1) {
+					// if process_start returns false, skip descending into the element
+					if (!process_start(node)) {
+						if (node.nextSibling) {
+							node = node.nextSibling;
+							continue start;
+						}
+					}
+				}
+			}
+			
+			if (node.firstChild) {
+				node = node.firstChild;
+				continue start;
+			}
+			
+			while (node) {
+			
+				// only interested in processing end element nodes
+				if (node.nodeType == 1) {
+					if (node == root) {
+						if (toc.entries.length == 0) {
+							toc.entries = null;
+						}
+						_toc = (toc.entries !== null && toc.entries.length > 0) ? JSON.stringify(toc,null,3) : null;
+					}
+					else {
+						process_end(node, root);
+					}
+				}
+				
+				if (node == root) {
+					node = null;
+				}
+				
+				else if (node.nextSibling) {
+					node = node.nextSibling;
+					continue start;
+				}
+				
+				else {
+					node = node.parentNode;
+				}
+			}
+			
+			
+			function process_start(node) {
+			
+				var lc_element = node.localName.toLowerCase();
+				
+				// set toc name to the heading if undefined and no branches have been created yet
+				if (lc_element.match(/^h[1-6]$/)) {
+					if (toc.name === '' && stack.length == 0) {
+						var label = node.textContent.trim();
+						toc.name = label ? label : null;
+					}
+					return false;
+				}
+				
+				else if (lc_element.match(/^[ou]l$/)) {
+					// the toc does not have a heading if one is not discovered before the first list
+					if (toc.name === '') {
+						toc.name = null;
+					}
+					
+					if (current_toc_branch !== null) {
+						// skip processing if this is an additional list within a branch
+						if (current_toc_branch.entries !== null && current_toc_branch.entries.length > 0) {
+							return false;
+						}
+						else {
+							stack.push(Object.assign({},current_toc_branch));
+						}
+					}
+					
+					else {
+						if (stack.length == 0) {
+							// skip processing if this is an additional list at the root of the nav element
+							if (toc.entries !== null && toc.entries.length > 0) {
+								return false;
+							}
+						}
+					}
+				}
+				
+				// create a new object when encountering a list item and push on current_toc_branch
+				else if (lc_element == 'li') {
+					
+					var new_branch = new Object();
+						new_branch.name = '';
+						new_branch.url = '';
+						new_branch.type = '';
+						new_branch.rel = '';
+						new_branch.entries = new Array();
+					
+					current_toc_branch = new_branch;
+				}
+				
+				// set the current current_toc_branch name and url properties when encountering an a tag
+				else if (lc_element == 'a') {
+					if (current_toc_branch !== null) {
+						if (current_toc_branch.name === '') {
+							
+							var label = node.textContent.trim();
+							current_toc_branch.name = (label !== '') ? label : null;
+							
+							current_toc_branch.url = node.hasAttribute('href') ? node.getAttribute('href') : null;
+							
+							var type = node.hasAttribute('type') ? node.getAttribute('type').trim() : null;
+							current_toc_branch.type = type ? type : null;
+							
+							var rel = node.hasAttribute('rel') ? node.getAttribute('rel').trim() : null;
+							current_toc_branch.rel = rel ? rel : null;
+						}
+					}
+					return false;
+				}
+				
+				// don't descend into sectioning and sectioning root elements
+				else if (lc_element == 'aside' ||
+					lc_element == 'article' ||
+					lc_element == 'section' ||
+					lc_element == 'nav' ||
+					lc_element == 'blockquote' ||
+					lc_element == 'details' ||
+					lc_element == 'dialog' ||
+					lc_element == 'fieldset' ||
+					lc_element == 'figure' ||
+					lc_element == 'td' ||
+					node.hidden) {
+					return false;
+				}
+				
+				return true;
+			}
+			
+			function process_end(node,root) {
+				
+				var lc_element = node.localName.toLowerCase();
+				
+				if (lc_element.match(/^[ou]l$/)) {
+					if (stack.length > 0) {
+						current_toc_branch = stack.pop();
+					}
+				}
+				
+				else if (lc_element == 'li') {
+					
+					// if the entries array is empty after processing the branch, set it to null
+					if (current_toc_branch.entries.length == 0) {
+						current_toc_branch.entries = null;
+					}
+					
+					// if there are active branches being created, the finished branch gets added into current_toc_branch
+					// otherwise, if there are no active branches it gets added to the root toc object's entries array
+					if (stack.length > 0) {
+						
+						if (current_toc_branch.name === '') {
+							
+							// if the branch has no name but subentries, set the name to null,
+							// otherwise, discard the branch if it doesn't have a name or any subentries
+							if (current_toc_branch.length > 0) {
+								current_toc_branch.name = null;
+							}
+							
+							else {
+								current_toc_branch = null;
+								return;
+							}
+						}
+						
+						// add the active branch to the entries array of its parent
+						stack[stack.length-1].entries.push(Object.assign({},current_toc_branch));
+					}
+					else {
+						toc.entries.push(Object.assign({},current_toc_branch));
+					}
+					
+					// discard the active branch now that processing is complete
+					current_toc_branch = null;
+				}
+			}
+		}
+	}
+	
+	
+	function getTOC(toc_url) {
+		$.ajax({
+			url:       toc_url,
+			cache:     false,
+			success: function(toc_page) {
+		    	var parser = new DOMParser();
+				var htmldoc = parser.parseFromString(toc_page, "text/html");
+				
+				var toc_element = htmldoc.querySelector('nav[role~="doc-toc"]');
+				generateTOC(toc_element);
+			},
+			error: function(xhr, status, error) {
+				console.error('Table of contents ' + toc_url + ' not found or an error occurred retrieving.');
+			}
+		});
+	}
+	
 	return {
 		processManifest: function(init) {
 			return processManifest(init);
+		},
+		getInternalRep: function() {
+			return _internal_rep;
+		},
+		getIssues: function() {
+			return _issues;
+		},
+		getTOC: function() {
+			return _toc;
 		}
 	}
 
